37b699729f6e2d18e2d15ae7d54d8f12cac9b7c3
Aman Kumar <aman.kumar.connect@gmail.com>
2024-09-12 00:30:46 +0530

    Update script.js

--- script.js
+++ script.js
@@ -5,23 +5,35 @@ renderTodoList();
 function renderTodoList(){
     let todoListHtml = '';
 
-    todoList.forEach((todoObject,index) => {
+    todoList.forEach(function(todoObject,index){
         const {name,dueDate} = todoObject;
         
         const html = `
             <div>${name}</div>
             <div>${dueDate} </div>
-            <button onclick="
-                todoList.splice(${index},1);
-                renderTodoList();
-            " class="delete-todo-button">Delete</button>
+            <button class="delete-todo-button js-delete-todo-button">Delete</button>
         `;
         todoListHtml += html;
     });
 
     document.querySelector('.js-todo-list')
         .innerHTML = todoListHtml;
+    
+    // delete todo button eventlistener
+    document.querySelectorAll('.js-delete-todo-button')
+        .forEach((deleteButton, index) => {
+            deleteButton.addEventListener('click', () => {
+                todoList.splice(index,1);
+                renderTodoList();
+            });
+        })
 }
+
+// add todo button eventlistener
+document.querySelector('.js-add-todo-button')
+    .addEventListener('click',() => {
+        addTodo();
+    })
 
 function addTodo(){
     const inputElement = document.querySelector('.js-name-input');
